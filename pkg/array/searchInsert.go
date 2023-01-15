01fed1d3277147c2ee16e4b7a1d270d7c91ec923
lauxinchi <wayilau@gmail.com>
2023-01-15 20:37:02 +0800

    searchInseart

---
+++ pkg/array/searchInsert.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+//题35
+func searchInsert(nums []int, target int) int {
+
+	if len(nums) == 1 {
+		if target <= nums[0] {
+			return 0
+		} else {
+			return 1
+		}
+	}
+
+	low := 0
+	high := len(nums) - 1
+	middle := low + (high-low)/2 + 1 //防溢出
+
+	if target == nums[middle] {
+		return middle
+	}
+
+	if target > nums[middle] {
+		return middle + searchInsert(nums[middle:], target)
+	}
+
+	if target < nums[middle] {
+		return searchInsert(nums[:middle], target)
+	}
+
+	return middle
+
+}
+
+func main() {
+	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 5))
+}
